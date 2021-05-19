c0ee64fb2791a1c7f4d249d5a6d629f8e712e400
SJArendonk <SJArendonk@gmail.com>
2021-05-19 15:07:29 +0200

    Initial commit

--- samples/drivers/ublox_m8/src/main.c
+++ samples/drivers/ublox_m8/src/main.c
@@ -1,5 +1,9 @@
 #include <errno.h>
 #include <zephyr.h>
+<<<<<<< HEAD
+=======
+//#include <misc/printk.h>
+>>>>>>> ed5781e27d... Initial commit
 #include <device.h>
 #include <drivers/i2c.h>
 #include <string.h>
@@ -7,6 +11,7 @@
 #include <math.h>
 
 #define I2C_M8N_ADDR 0x42
+<<<<<<< HEAD
 #define I2C_M8N_REG_ADDR 0x00
 #define SUCCESS 0
 
@@ -164,3 +169,104 @@ void main(void)
 
 // 	return i2c_transfer(i2c_dev, &msgs[0], 2, I2C_M8N_ADDR);
 // }
+=======
+
+const struct device * i2c_dev;
+
+static void init(){
+
+	i2c_dev = device_get_binding("I2C_0");
+	if (!i2c_dev) {
+		printk("I2C: Device driver not found.\n");
+		return;
+	} 
+	else {
+		i2c_configure(i2c_dev, I2C_SPEED_SET(I2C_SPEED_FAST));
+		return;
+    }
+}
+
+static int write_bytes(const struct device *i2c_dev, uint16_t addr,
+		       uint8_t *data, uint32_t num_bytes)
+{
+	uint8_t wr_addr[2];
+	struct i2c_msg msgs[2];
+
+	/* FRAM address */
+	wr_addr[0] = (addr >> 8) & 0xFF;
+	wr_addr[1] = addr & 0xFF;
+
+	/* Setup I2C messages */
+
+	/* Send the address to write to */
+	msgs[0].buf = wr_addr;
+	msgs[0].len = 2U;
+	msgs[0].flags = I2C_MSG_WRITE;
+
+	/* Data to be written, and STOP after this. */
+	msgs[1].buf = data;
+	msgs[1].len = num_bytes;
+	msgs[1].flags = I2C_MSG_WRITE | I2C_MSG_STOP;
+
+	return i2c_transfer(i2c_dev, &msgs[0], 2, I2C_M8N_ADDR);
+}
+
+static int read_bytes(const struct device *i2c_dev, uint16_t addr,
+		      uint8_t *data, uint32_t num_bytes)
+{
+	uint8_t wr_addr[2];
+	struct i2c_msg msgs[2];
+
+	/* Now try to read back from FRAM */
+
+	/* FRAM address */
+	wr_addr[0] = (addr >> 8) & 0xFF;
+	wr_addr[1] = addr & 0xFF;
+
+	/* Setup I2C messages */
+
+	/* Send the address to read from */
+	msgs[0].buf = wr_addr;
+	msgs[0].len = 2U;
+	msgs[0].flags = I2C_MSG_WRITE;
+
+	/* Read from device. STOP after this. */
+	msgs[1].buf = data;
+	msgs[1].len = num_bytes;
+	msgs[1].flags = I2C_MSG_READ | I2C_MSG_STOP;
+
+	return i2c_transfer(i2c_dev, &msgs[0], 2, I2C_M8N_ADDR);
+}
+
+void main(void)
+{
+        printk("Test 1, 2, 3.\n");
+        
+	//uint8_t cmp_data[16];
+	uint8_t data[16];
+	int ret;
+
+		init();
+
+	/* Do one-byte read/write */
+
+	data[0] = 0xFF;
+	ret = write_bytes(i2c_dev, 0x00, &data[0], 1);
+	if (ret) {
+		printk("Error writing to FRAM! error code (%d)\n", ret);
+		return;
+	} else {
+		printk("Wrote 0xAE to address 0x00.\n");
+	}
+
+	data[0] = 0x00;
+	ret = read_bytes(i2c_dev, 0x00, &data[0], 1);
+	if (ret) {
+		printk("Error reading from FRAM! error code (%d)\n", ret);
+		return;
+	} else {
+		printk("Read 0x%X from address 0x00.\n", data[0]);
+	}
+	
+}
+>>>>>>> ed5781e27d... Initial commit
