0e052dc9189d9b7a3062d5683f9914a2b9e742a5
Imre Lagas <Imre_Lagas@hotmail.com>
2021-06-15 10:36:38 +0200

    i2c should work but not tested with m8

--- drivers/sensor/neo_m8/neo_m8.c
+++ drivers/sensor/neo_m8/neo_m8.c
@@ -22,8 +22,8 @@
 
 /* Device constant configuration parameters */
 struct ublox_m8_dev_cfg {
-	const struct soc_gpio_pin *pin_list;
-	uint8_t pin_list_size;
+	uint8_t i2c_slave_addr;
+	const struct device *i2c_master;	
 };
 
 /* Device run time data */
@@ -50,8 +50,24 @@ static int ublox_m8_get(const struct device *dev, enum sensor_channel chan,
 static int ublox_m8_initialize(const struct device *dev)
 {
 	__ASSERT_NO_MSG(dev != NULL);
-	
-printk("\n init m8!");
+
+	struct ublox_m8_dev_cfg *const config = (struct ublox_m8_dev_cfg *)dev->config;
+	struct ublox_m8_dev_data *data = dev->data;
+
+	if (config->i2c_slave_addr == 0) {
+		printk("No I2C slave adres in neo-m8 init!\n");
+		return EINVAL;
+	}
+
+	if (config->i2c_master == NULL) {
+		printk("No I2C master in neo-m8 init!\n");
+		return EINVAL;
+	}
+
+	volatile int a = config->i2c_slave_addr;
+	volatile int b = config->i2c_master;
+
+printk("\n");
 	// /* Connect pins to the peripheral */
 	// soc_gpio_list_configure(dev_cfg->pin_list, dev_cfg->pin_list_size);
 
@@ -70,6 +86,8 @@ static const struct sensor_driver_api ublox_m8_driver_api = {
 };
 
 static const struct ublox_m8_dev_cfg m8inst_sam_config = {	
+	.i2c_slave_addr = DT_INST_REG_ADDR(0),                               
+	.i2c_master = DEVICE_DT_GET(DT_INST_BUS(0))                          
 	};								
 							
 	static struct ublox_m8_dev_data m8inst_sam_data;		
